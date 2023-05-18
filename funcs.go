e7dbfb9018915f43e64eee01192a7529c1d41e17
whencome <twqzy323@163.com>
2023-05-18 16:50:15 +0800

    add Cond functions

--- funcs.go
+++ funcs.go
@@ -1,155 +1,126 @@
 package gotil
 
 import (
-	"fmt"
-	"math/rand"
-	"reflect"
-	"strconv"
-	"strings"
-	"time"
-	"unicode/utf8"
+    "fmt"
+    "math/rand"
+    "reflect"
+    "strconv"
+    "strings"
+    "time"
+    "unicode/utf8"
 
-	"github.com/axgle/mahonia"
+    "github.com/axgle/mahonia"
 )
 
 // 定义数据表特殊字符替换映射表
 var sqlSpecialCharMaps = []map[string]string{
-	{"old": `\`, "new": `\\`},
-	{"old": `'`, "new": `\'`},
-	{"old": `"`, "new": `\"`},
+    {"old": `\`, "new": `\\`},
+    {"old": `'`, "new": `\'`},
+    {"old": `"`, "new": `\"`},
 }
 
 // EscapeSqlValue 转义数据库中的特殊字符，暂时只处理常见内容
 func EscapeSqlValue(str string) string {
-	// 先检查是否是utf8，不是则先转换
-	if !utf8.ValidString(str) {
-		utf8Encoder := mahonia.NewEncoder("UTF-8")
-		str = utf8Encoder.ConvertString(str)
-	}
-	for _, repl := range sqlSpecialCharMaps {
-		str = strings.ReplaceAll(str, repl["old"], repl["new"])
-	}
-	return str
+    // 先检查是否是utf8，不是则先转换
+    if !utf8.ValidString(str) {
+        utf8Encoder := mahonia.NewEncoder("UTF-8")
+        str = utf8Encoder.ConvertString(str)
+    }
+    for _, repl := range sqlSpecialCharMaps {
+        str = strings.ReplaceAll(str, repl["old"], repl["new"])
+    }
+    return str
 }
 
 ////////////////// UTIL FUNCS ///////////////
+
 // CopyStruct 复制结构体
 func CopyStruct(src, dst interface{}) {
-	dstVal := reflect.ValueOf(dst).Elem() //获取reflect.Type类型
-	srcVal := reflect.ValueOf(src).Elem() //获取reflect.Type类型
-	vTypeOfT := srcVal.Type()
-	for i := 0; i < srcVal.NumField(); i++ {
-		// 在要修改的结构体中查询有数据结构体中相同属性的字段，有则修改其值
-		name := vTypeOfT.Field(i).Name
-		// 检查目标struct是否存在name
-		if reflect.DeepEqual(dstVal.FieldByName(name), reflect.Value{}) {
-			continue
-		}
-		dFieldType := dstVal.FieldByName(name).Type()
-		sFieldType := srcVal.FieldByName(name).Type()
-		// 名字及类型一致，才能赋值
-		if ok := dstVal.FieldByName(name).IsValid(); ok && sFieldType.AssignableTo(dFieldType) {
-			dstVal.FieldByName(name).Set(reflect.ValueOf(srcVal.Field(i).Interface()))
-		}
-	}
+    dstVal := reflect.ValueOf(dst).Elem() //获取reflect.Type类型
+    srcVal := reflect.ValueOf(src).Elem() //获取reflect.Type类型
+    vTypeOfT := srcVal.Type()
+    for i := 0; i < srcVal.NumField(); i++ {
+        // 在要修改的结构体中查询有数据结构体中相同属性的字段，有则修改其值
+        name := vTypeOfT.Field(i).Name
+        // 检查目标struct是否存在name
+        if reflect.DeepEqual(dstVal.FieldByName(name), reflect.Value{}) {
+            continue
+        }
+        dFieldType := dstVal.FieldByName(name).Type()
+        sFieldType := srcVal.FieldByName(name).Type()
+        // 名字及类型一致，才能赋值
+        if ok := dstVal.FieldByName(name).IsValid(); ok && sFieldType.AssignableTo(dFieldType) {
+            dstVal.FieldByName(name).Set(reflect.ValueOf(srcVal.Field(i).Interface()))
+        }
+    }
 }
 
-// 获取随机字符串
+// RandString 获取随机字符串
 func RandString(len int) string {
-	r := rand.New(rand.NewSource(time.Now().Unix()))
-	bytes := make([]byte, len)
-	for i := 0; i < len; i++ {
-		b := r.Intn(26) + 65
-		bytes[i] = byte(b)
-	}
-	return string(bytes)
+    r := rand.New(rand.NewSource(time.Now().Unix()))
+    bytes := make([]byte, len)
+    for i := 0; i < len; i++ {
+        b := r.Intn(26) + 65
+        bytes[i] = byte(b)
+    }
+    return string(bytes)
 }
 
 // IsNil 判断给定的值是否为nil
 func IsNil(i interface{}) bool {
-	ret := i == nil
-	// 需要进一步做判断
-	if !ret {
-		vi := reflect.ValueOf(i)
-		kind := reflect.ValueOf(i).Kind()
-		if kind == reflect.Slice ||
-			kind == reflect.Map ||
-			kind == reflect.Chan ||
-			kind == reflect.Interface ||
-			kind == reflect.Func ||
-			kind == reflect.Ptr {
-			return vi.IsNil()
-		}
-	}
-	return ret
+    ret := i == nil
+    // 需要进一步做判断
+    if !ret {
+        vi := reflect.ValueOf(i)
+        kind := reflect.ValueOf(i).Kind()
+        if kind == reflect.Slice ||
+            kind == reflect.Map ||
+            kind == reflect.Chan ||
+            kind == reflect.Interface ||
+            kind == reflect.Func ||
+            kind == reflect.Ptr {
+            return vi.IsNil()
+        }
+    }
+    return ret
 }
 
-// 字符串截取
+// SubString 字符串截取
 func SubString(str string, start, end int) string {
-	rs := []rune(str)
-	return string(rs[start:end])
+    rs := []rune(str)
+    return string(rs[start:end])
 }
 
-// 判断是否为空
+// IsEmpty 判断是否为空
 func IsEmpty(v interface{}) bool {
-	if v == nil {
-		return true
-	}
-	switch v.(type) {
-	case int:
-		val := v.(int)
-		return val == 0
-	case int8:
-		val := v.(int8)
-		return val == 0
-	case int16:
-		val := v.(int16)
-		return val == 0
-	case int32:
-		val := v.(int32)
-		return val == 0
-	case int64:
-		val := v.(int64)
-		return val == 0
-	case uint:
-		val := v.(uint)
-		return val == 0
-	case uint8:
-		val := v.(uint8)
-		return val == 0
-	case uint16:
-		val := v.(uint16)
-		return val == 0
-	case uint32:
-		val := v.(uint32)
-		return val == 0
-	case uint64:
-		val := v.(uint64)
-		return val == 0
-	case float32:
-		val := v.(float32)
-		return val == 0
-	case float64:
-		val := v.(float64)
-		return val == 0
-	case string:
-		val := v.(string)
-		if val == "" {
-			return true
-		}
-	case []byte:
-		val := v.([]byte)
-		return len(val) == 0
-	case []rune:
-		val := v.([]rune)
-		return len(val) == 0
-	case bool:
-		val := v.(bool)
-		return !val
-	default:
-		return IsNil(v)
-	}
-	return false
+    if v == nil {
+        return true
+    }
+    return reflect.ValueOf(v).IsZero()
+}
+
+// Cond 条件表达式，用于实现三元运算符的功能，if cond == true then trueVal else falseVal
+func Cond(cond bool, trueVal interface{}, falseVal interface{}) interface{} {
+    if cond {
+        return trueVal
+    }
+    return falseVal
+}
+
+// NilCond 判断值v，如果等于nil，则返回elseVal，否则返回v本身
+func NilCond(v interface{}, elseVal interface{}) interface{} {
+    if v == nil {
+        return elseVal
+    }
+    return v
+}
+
+// EmptyCond 判断值v，如果为空（零值或者nil），则返回elseVal，否则返回v本身
+func EmptyCond(v interface{}, elseVal interface{}) interface{} {
+    if IsEmpty(v) {
+        return elseVal
+    }
+    return v
 }
 
 /////////////////////// TYPE CONVERSIONS ///////////////////////////
@@ -158,204 +129,204 @@ func IsEmpty(v interface{}) bool {
 // 注意：1. 当无法或者不支持转换时，此接口将返回0值；
 //       2. 此接口应当只在明确数据内容只是需要类型转换时使用，不应当用于对未知值进行转换。
 func String(v interface{}) string {
-	var strVal = ""
-	switch v.(type) {
-	case int, int8, int16, int32, int64:
-		n := Int64(v)
-		strVal = strconv.FormatInt(n, 10)
-	case uint, uint8, uint16, uint32, uint64:
-		n := Uint64(v)
-		strVal = strconv.FormatUint(n, 10)
-	case float32:
-		strVal = strconv.FormatFloat(float64(v.(float32)), 'f', -1, 64)
-	case float64:
-		strVal = strconv.FormatFloat(v.(float64), 'f', -1, 64)
-	case string:
-		strVal = v.(string)
-	case []byte:
-		strVal = string(v.([]byte))
-	case []rune:
-		strVal = string(v.([]rune))
-	case bool:
-		strVal = strconv.FormatBool(v.(bool))
-	default:
-		if v == nil {
-			strVal = ""
-		} else {
-			strVal = fmt.Sprint(v)
-		}
-	}
-	return strVal
+    var strVal = ""
+    switch v.(type) {
+    case int, int8, int16, int32, int64:
+        n := Int64(v)
+        strVal = strconv.FormatInt(n, 10)
+    case uint, uint8, uint16, uint32, uint64:
+        n := Uint64(v)
+        strVal = strconv.FormatUint(n, 10)
+    case float32:
+        strVal = strconv.FormatFloat(float64(v.(float32)), 'f', -1, 64)
+    case float64:
+        strVal = strconv.FormatFloat(v.(float64), 'f', -1, 64)
+    case string:
+        strVal = v.(string)
+    case []byte:
+        strVal = string(v.([]byte))
+    case []rune:
+        strVal = string(v.([]rune))
+    case bool:
+        strVal = strconv.FormatBool(v.(bool))
+    default:
+        if v == nil {
+            strVal = ""
+        } else {
+            strVal = fmt.Sprint(v)
+        }
+    }
+    return strVal
 }
 
 // Int64 将任意值转换成int64类型
 // 注意：1. 当无法或者不支持转换时，此接口将返回0值；
 //       2. 此接口应当只在明确数据内容只是需要类型转换时使用，不应当用于对未知值进行转换。
 func Int64(v interface{}) int64 {
-	switch v.(type) {
-	case int:
-		return int64(v.(int))
-	case int8:
-		return int64(v.(int8))
-	case int16:
-		return int64(v.(int16))
-	case int32:
-		return int64(v.(int32))
-	case int64:
-		return v.(int64)
-	case uint:
-		return int64(v.(uint))
-	case uint8:
-		return int64(v.(uint8))
-	case uint16:
-		return int64(v.(uint16))
-	case uint32:
-		return int64(v.(uint32))
-	case uint64:
-		return int64(v.(uint64))
-	case float32:
-		return int64(v.(float32))
-	case float64:
-		return int64(v.(float64))
-	case string:
-		n, err := strconv.ParseInt(string(v.(string)), 10, 64)
-		if err != nil {
-			return 0
-		}
-		return n
-	case bool:
-		if v.(bool) {
-			return 1
-		}
-		return 0
-	default:
-		return 0
-	}
-	return 0
+    switch v.(type) {
+    case int:
+        return int64(v.(int))
+    case int8:
+        return int64(v.(int8))
+    case int16:
+        return int64(v.(int16))
+    case int32:
+        return int64(v.(int32))
+    case int64:
+        return v.(int64)
+    case uint:
+        return int64(v.(uint))
+    case uint8:
+        return int64(v.(uint8))
+    case uint16:
+        return int64(v.(uint16))
+    case uint32:
+        return int64(v.(uint32))
+    case uint64:
+        return int64(v.(uint64))
+    case float32:
+        return int64(v.(float32))
+    case float64:
+        return int64(v.(float64))
+    case string:
+        n, err := strconv.ParseInt(string(v.(string)), 10, 64)
+        if err != nil {
+            return 0
+        }
+        return n
+    case bool:
+        if v.(bool) {
+            return 1
+        }
+        return 0
+    default:
+        return 0
+    }
+    return 0
 }
 
 func Int32(v interface{}) int32 {
-	return int32(Int64(v))
+    return int32(Int64(v))
 }
 
 func Int(v interface{}) int {
-	return int(Int64(v))
+    return int(Int64(v))
 }
 
 // Uint64 将任意值转换成uint64类型
 // 注意：1. 当无法或者不支持转换时，此接口将返回0值；
 //       2. 此接口应当只在明确数据内容只是需要类型转换时使用，不应当用于对未知值进行转换。
 func Uint64(v interface{}) uint64 {
-	switch v.(type) {
-	case int:
-		return uint64(v.(int))
-	case int8:
-		return uint64(v.(int8))
-	case int16:
-		return uint64(v.(int16))
-	case int32:
-		return uint64(v.(int32))
-	case int64:
-		return uint64(v.(int64))
-	case uint:
-		return uint64(v.(uint))
-	case uint8:
-		return uint64(v.(uint8))
-	case uint16:
-		return uint64(v.(uint16))
-	case uint32:
-		return uint64(v.(uint32))
-	case uint64:
-		return v.(uint64)
-	case float32:
-		return uint64(v.(float32))
-	case float64:
-		return uint64(v.(float64))
-	case string:
-		n, err := strconv.ParseUint(string(v.(string)), 10, 64)
-		if err != nil {
-			return 0
-		}
-		return n
-	case bool:
-		if v.(bool) {
-			return 1
-		}
-		return 0
-	default:
-		return 0
-	}
-	return 0
+    switch v.(type) {
+    case int:
+        return uint64(v.(int))
+    case int8:
+        return uint64(v.(int8))
+    case int16:
+        return uint64(v.(int16))
+    case int32:
+        return uint64(v.(int32))
+    case int64:
+        return uint64(v.(int64))
+    case uint:
+        return uint64(v.(uint))
+    case uint8:
+        return uint64(v.(uint8))
+    case uint16:
+        return uint64(v.(uint16))
+    case uint32:
+        return uint64(v.(uint32))
+    case uint64:
+        return v.(uint64)
+    case float32:
+        return uint64(v.(float32))
+    case float64:
+        return uint64(v.(float64))
+    case string:
+        n, err := strconv.ParseUint(string(v.(string)), 10, 64)
+        if err != nil {
+            return 0
+        }
+        return n
+    case bool:
+        if v.(bool) {
+            return 1
+        }
+        return 0
+    default:
+        return 0
+    }
+    return 0
 }
 
 func Uint32(v interface{}) uint32 {
-	return uint32(Uint64(v))
+    return uint32(Uint64(v))
 }
 
 func Uint(v interface{}) uint {
-	return uint(Uint64(v))
+    return uint(Uint64(v))
 }
 
 // Float64 将任意值转换成float64类型
 // 注意：1. 当无法或者不支持转换时，此接口将返回0值；
 //       2. 此接口应当只在明确数据内容只是需要类型转换时使用，不应当用于对未知值进行转换。
 func Float64(v interface{}) float64 {
-	switch v.(type) {
-	case int:
-		return float64(v.(int))
-	case int8:
-		return float64(v.(int8))
-	case int16:
-		return float64(v.(int16))
-	case int32:
-		return float64(v.(int32))
-	case int64:
-		return float64(v.(int64))
-	case uint:
-		return float64(v.(uint))
-	case uint8:
-		return float64(v.(uint8))
-	case uint16:
-		return float64(v.(uint16))
-	case uint32:
-		return float64(v.(uint32))
-	case uint64:
-		return float64(v.(uint64))
-	case float32:
-		return float64(v.(float32))
-	case float64:
-		return float64(v.(float64))
-	case string:
-		n, err := strconv.ParseFloat(string(v.(string)), 64)
-		if err != nil {
-			return 0
-		}
-		return n
-	case bool:
-		if v.(bool) {
-			return 1
-		}
-		return 0
-	default:
-		return 0
-	}
-	return 0
+    switch v.(type) {
+    case int:
+        return float64(v.(int))
+    case int8:
+        return float64(v.(int8))
+    case int16:
+        return float64(v.(int16))
+    case int32:
+        return float64(v.(int32))
+    case int64:
+        return float64(v.(int64))
+    case uint:
+        return float64(v.(uint))
+    case uint8:
+        return float64(v.(uint8))
+    case uint16:
+        return float64(v.(uint16))
+    case uint32:
+        return float64(v.(uint32))
+    case uint64:
+        return float64(v.(uint64))
+    case float32:
+        return float64(v.(float32))
+    case float64:
+        return float64(v.(float64))
+    case string:
+        n, err := strconv.ParseFloat(string(v.(string)), 64)
+        if err != nil {
+            return 0
+        }
+        return n
+    case bool:
+        if v.(bool) {
+            return 1
+        }
+        return 0
+    default:
+        return 0
+    }
+    return 0
 }
 
 func Float32(v interface{}) float32 {
-	return float32(Float64(v))
+    return float32(Float64(v))
 }
 
 // Bool 将任意值转换成bool类型
 // 注意：1. 当无法或者不支持转换时，此接口将返回0值；
 //       2. 此接口应当只在明确数据内容只是需要类型转换时使用，不应当用于对未知值进行转换。
 func Bool(v interface{}) bool {
-	if b, ok := v.(bool); ok {
-		return b
-	}
-	iv := Uint64(v)
-	if iv > 0 {
-		return true
-	}
-	return false
+    if b, ok := v.(bool); ok {
+        return b
+    }
+    iv := Uint64(v)
+    if iv > 0 {
+        return true
+    }
+    return false
 }
--- map2struct/map2struct.go
+++ map2struct/map2struct.go
@@ -1,211 +1,161 @@
 package map2struct
 
 import (
-	"fmt"
-	"reflect"
+    "fmt"
+    "reflect"
 
-	"github.com/whencome/gotil"
+    "github.com/whencome/gotil"
 )
 
 // 定义转换器
 type converter struct {
-	Object       interface{}       // 转换目标struct
-	Tag          string            // 标签名称
-	PropFieldMap map[string]string // 属性与字段(tag定义)映射关系
-	FieldPropMap map[string]string // 字段与属性映射关系
-	PropValues   gotil.M           // 属性与值的映射关系
+    Object       interface{}       // 转换目标struct
+    Tag          string            // 标签名称
+    PropFieldMap map[string]string // 属性与字段(tag定义)映射关系
+    FieldPropMap map[string]string // 字段与属性映射关系
+    PropValues   gotil.M           // 属性与值的映射关系
 }
 
 // 创建一个转换器
 func newConverter(obj interface{}, tag string) *converter {
-	c := &converter{
-		Object:       obj,
-		Tag:          tag,
-		PropFieldMap: make(map[string]string),
-		FieldPropMap: make(map[string]string),
-		PropValues:   gotil.M{},
-	}
-	c.init()
-	return c
+    c := &converter{
+        Object:       obj,
+        Tag:          tag,
+        PropFieldMap: make(map[string]string),
+        FieldPropMap: make(map[string]string),
+        PropValues:   gotil.M{},
+    }
+    c.init()
+    return c
 }
 
 // 初始化转换器，提取字段信息
 func (c *converter) init() {
-	// 获取tag中的内容
-	rt := reflect.TypeOf(c.Object)
-	if rt.Kind() == reflect.Ptr {
-		rt = rt.Elem()
-	}
-	rv := reflect.ValueOf(c.Object)
-	if rv.Kind() == reflect.Ptr {
-		rv = rv.Elem()
-	}
-	// 获取字段数量
-	fieldsNum := rt.NumField()
-	for i := 0; i < fieldsNum; i++ {
-		field := rt.Field(i)
-		propName := field.Name
-		fieldName := ""
-		// 如果没有指定tag，则使用属性本身作为字段名
-		if c.Tag == "" {
-			fieldName = propName
-		} else {
-			fieldName = field.Tag.Get(c.Tag)
-			if fieldName == "" || fieldName == "-" {
-				continue
-			}
-		}
-		c.PropFieldMap[propName] = fieldName
-		c.FieldPropMap[fieldName] = propName
-		// 获取对应字段的值
-		reflectField := rv.Field(i)
-		c.PropValues[propName] = reflectField.Interface()
-	}
+    // 获取tag中的内容
+    rt := reflect.TypeOf(c.Object)
+    if rt.Kind() == reflect.Ptr {
+        rt = rt.Elem()
+    }
+    rv := reflect.ValueOf(c.Object)
+    if rv.Kind() == reflect.Ptr {
+        rv = rv.Elem()
+    }
+    // 获取字段数量
+    fieldsNum := rt.NumField()
+    for i := 0; i < fieldsNum; i++ {
+        field := rt.Field(i)
+        propName := field.Name
+        fieldName := ""
+        // 如果没有指定tag，则使用属性本身作为字段名
+        if c.Tag == "" {
+            fieldName = propName
+        } else {
+            fieldName = field.Tag.Get(c.Tag)
+            if fieldName == "" || fieldName == "-" {
+                continue
+            }
+        }
+        c.PropFieldMap[propName] = fieldName
+        c.FieldPropMap[fieldName] = propName
+        // 获取对应字段的值
+        reflectField := rv.Field(i)
+        c.PropValues[propName] = reflectField.Interface()
+    }
 }
 
-// 将对象转换为map
+// ToMap 将对象转换为map
 func (c *converter) ToMap() gotil.M {
-	// 创建m对象
-	m := gotil.M{}
-	for propName, value := range c.PropValues {
-		fieldName, ok := c.PropFieldMap[propName]
-		if !ok {
-			continue
-		}
-		m[fieldName] = value
-	}
-	return m
+    // 创建m对象
+    m := gotil.M{}
+    for propName, value := range c.PropValues {
+        fieldName, ok := c.PropFieldMap[propName]
+        if !ok {
+            continue
+        }
+        m[fieldName] = value
+    }
+    return m
 }
 
-// 将map赋值到struct
+// ToStruct 将map赋值到struct
 func (c *converter) ToStruct(v interface{}) error {
-	// 将数据转换成map
-	mapData, err := getMapData(v)
-	if err != nil {
-		return err
-	}
-	if len(c.PropFieldMap) == 0 {
-		return nil
-	}
-	// 创建对象并进行转换
-	rv := reflect.ValueOf(c.Object)
-	if rv.Kind() == reflect.Ptr {
-		rv = rv.Elem()
-	}
-	for fieldName, propName := range c.FieldPropMap {
-		// 检查在map中是否存在此值
-		v, ok := mapData[fieldName]
-		if !ok {
-			continue
-		}
-		// 设置值
-		reflectField := rv.FieldByName(propName)
-		propTypeKind := reflectField.Kind()
-		switch propTypeKind {
-		case reflect.String:
-			reflectField.SetString(gotil.String(v))
-		case reflect.Bool:
-			reflectField.SetBool(gotil.Bool(v))
-		case reflect.Int64, reflect.Int, reflect.Int32, reflect.Int16, reflect.Int8:
-			reflectField.SetInt(gotil.Int64(v))
-		case reflect.Uint64, reflect.Uint, reflect.Uint32, reflect.Uint16, reflect.Uint8:
-			reflectField.SetUint(gotil.Uint64(v))
-		case reflect.Float64:
-			reflectField.SetFloat(gotil.Float64(v))
-		default: // 其他类型暂不支持
-			break
-		}
-	}
-	return nil
+    // 将数据转换成map
+    mapData, err := getMapData(v)
+    if err != nil {
+        return err
+    }
+    if len(c.PropFieldMap) == 0 {
+        return nil
+    }
+    // 创建对象并进行转换
+    rv := reflect.ValueOf(c.Object)
+    if rv.Kind() == reflect.Ptr {
+        rv = rv.Elem()
+    }
+    for fieldName, propName := range c.FieldPropMap {
+        // 检查在map中是否存在此值
+        v, ok := mapData[fieldName]
+        if !ok {
+            continue
+        }
+        // 设置值
+        reflectField := rv.FieldByName(propName)
+        propTypeKind := reflectField.Kind()
+        switch propTypeKind {
+        case reflect.String:
+            reflectField.SetString(gotil.String(v))
+        case reflect.Bool:
+            reflectField.SetBool(gotil.Bool(v))
+        case reflect.Int64, reflect.Int, reflect.Int32, reflect.Int16, reflect.Int8:
+            reflectField.SetInt(gotil.Int64(v))
+        case reflect.Uint64, reflect.Uint, reflect.Uint32, reflect.Uint16, reflect.Uint8:
+            reflectField.SetUint(gotil.Uint64(v))
+        case reflect.Float64:
+            reflectField.SetFloat(gotil.Float64(v))
+        default: // 其他类型暂不支持
+            break
+        }
+    }
+    return nil
 }
 
 // getMapData 将给定的参数转换成指定的map格式，如果参数不是支持的类型将报错
 func getMapData(data interface{}) (gotil.M, error) {
-	retData := gotil.M{}
-	switch data.(type) {
-	case map[string]interface{}:
-		convertedData := data.(map[string]interface{})
-		for k, v := range convertedData {
-			retData[k] = v
-		}
-	case map[string]string:
-		convertedData := data.(map[string]string)
-		for k, v := range convertedData {
-			retData[k] = v
-		}
-	case map[string]int:
-		convertedData := data.(map[string]int)
-		for k, v := range convertedData {
-			retData[k] = v
-		}
-	case map[string]int64:
-		convertedData := data.(map[string]int64)
-		for k, v := range convertedData {
-			retData[k] = v
-		}
-	case map[string]int32:
-		convertedData := data.(map[string]int32)
-		for k, v := range convertedData {
-			retData[k] = v
-		}
-	case map[string]int16:
-		convertedData := data.(map[string]int16)
-		for k, v := range convertedData {
-			retData[k] = v
-		}
-	case map[string]uint:
-		convertedData := data.(map[string]uint)
-		for k, v := range convertedData {
-			retData[k] = v
-		}
-	case map[string]uint64:
-		convertedData := data.(map[string]uint64)
-		for k, v := range convertedData {
-			retData[k] = v
-		}
-	case map[string]uint32:
-		convertedData := data.(map[string]uint32)
-		for k, v := range convertedData {
-			retData[k] = v
-		}
-	case map[string]float32:
-		convertedData := data.(map[string]float32)
-		for k, v := range convertedData {
-			retData[k] = v
-		}
-	case map[string]float64:
-		convertedData := data.(map[string]float64)
-		for k, v := range convertedData {
-			retData[k] = v
-		}
-	case gotil.M:
-		retData = data.(gotil.M)
-	default:
-		return nil, fmt.Errorf("unsupported data type %T of %#v", data, data)
-	}
-	return retData, nil
+    if dm, ok := data.(gotil.M); ok {
+        return dm, nil
+    }
+    retData := gotil.M{}
+    value := reflect.ValueOf(data)
+    if value.Kind() == reflect.Map {
+        for _, k := range value.MapKeys() {
+            retData[k.String()] = value.MapIndex(k).Interface()
+        }
+    } else {
+        return nil, fmt.Errorf("unsupported data type %T of %#v", data, data)
+    }
+    return retData, nil
 }
 
 // ToStructByTag 将map数据（data）填充到给定的结构体（obj）中，根据指定的tag解析与转换
 func ToStructByTag(dstObj interface{}, mapData interface{}, tag string) error {
-	c := newConverter(dstObj, tag)
-	return c.ToStruct(mapData)
+    c := newConverter(dstObj, tag)
+    return c.ToStruct(mapData)
 }
 
 // ToStruct 将map数据（data）填充到给定的结构体（obj）中，根据指定的tag解析与转换
 func ToStruct(dstObj interface{}, mapData interface{}) error {
-	c := newConverter(dstObj, "map2struct")
-	return c.ToStruct(mapData)
+    c := newConverter(dstObj, "map2struct")
+    return c.ToStruct(mapData)
 }
 
 // ToMapByTag 将struct数据解析为map
 func ToMapByTag(dstObj interface{}, tag string) gotil.M {
-	c := newConverter(dstObj, tag)
-	return c.ToMap()
+    c := newConverter(dstObj, tag)
+    return c.ToMap()
 }
 
 // ToMap 将struct数据解析为map
 func ToMap(dstObj interface{}) gotil.M {
-	c := newConverter(dstObj, "map2struct")
-	return c.ToMap()
+    c := newConverter(dstObj, "map2struct")
+    return c.ToMap()
 }
